fa5724f0bd27b6779926eb028d5e0fff39049d38
112ab0058 <146065141+112ab0058@users.noreply.github.com>
2023-10-31 15:06:30 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int hours,salary;
+    scanf("%d%d",&hours,&salary);
+    float money;
+    if(hours<=60){
+        money=salary*hours;
+        printf("%.1f",money);
+    }else if(hours>60&&hours<=120){
+        money=(60*salary)+((hours-60)*salary*1.33);
+        printf("%.1f",money);
+    }else if(hours>120){
+        money=(60*salary)+(60*salary*1.33)+((hours-120)*salary*1.66);
+        printf("%.1f",money);
+        
+    }
+        
+    }
 
