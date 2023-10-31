08970c9dd3f370e77e11c6c2f928bb3ac97119b9
112ab0058 <146065141+112ab0058@users.noreply.github.com>
2023-10-31 15:05:09 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float C;
+    scanf("%f",&C);
+    float f = (C*9/5)+32;
+    printf("%.1f",f);
+}
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int min;
+    scanf("%d",&min);
+    float price;
+    if(min<=800){
+        price=min*0.9;
+    }else if(min>800&&min<1500){
+        price=min*0.9*0.9;
+    }else if(min>=1500){
+        price=min*0.9*0.79;
+    }
+    printf("%.1f",price);
+    }
 
