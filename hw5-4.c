beb64a43e3d748289ed407f46ff2a193bb681e0d
112ab0058 <146065141+112ab0058@users.noreply.github.com>
2023-10-31 15:40:31 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    int H,M;
+    scanf("%d%d",&H,&M);
+    if(M==0){
+        H=H*30;
+        if(H>180){
+            H=H-180;
+            printf("%d",H);
+        }
+        
+    }else{
+        float a,b;
+        b=(H*5-M)*6;
+        a=M*0.5;
+        
+        float ans =a+b;
+        printf("%.3f",ans);
+    }
+    
+        
+}
